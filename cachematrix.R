81483c2ce15810a45ba25b6df67b09d2a1e428ab
Ayres-Sampaio <diogo.ayres@gmail.com>
2014-08-23 14:43:19 +0100

    final

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,7 @@
 # The makeCacheMatrix function receives a matrix as input and creates a 
 # special matrix object. The returned object is basically a list with four 
 # functions that allows the user to access and change the object content:
-#    1) set:    Rewrites the object's matrix based on an new input matrix 
+#    1) set:    Rewrites the object's matrix based on a new input matrix 
 #               and sets its inverse to null
 #    2) get:    Returns the stored matrix
 #    3) setinv: Caches the inverse of the stored matrix
