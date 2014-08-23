1ade7441337e63060c5c05f4f35e418b7a15660e
Ayres-Sampaio <diogo.ayres@gmail.com>
2014-08-23 12:03:16 +0100

    final

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+# cachematrix.R file contains a sequence of two functions which receives a 
+# (square) matrix as input, creates a special matrix object, and computes its 
+# inverse
 
-## Write a short comment describing this function
+# The makeCacheMatrix function receives a matrix as input and creates a 
+# special matrix object. The returned object is basically a list with four 
+# functions that allows the user to access and change the object content:
+#    1) set:    caches the input matrix and sets the inverse to null
+#    2) get:    returns the stored input matrix
+#    3) setinv: caches the inverse of the input matrix
+#    4) getinv: returns the inverse of the input matrix
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
@@ -15,10 +22,14 @@ makeCacheMatrix <- function(x = matrix()) {
         list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-## Write a short comment describing this function
+# The cacheSolve function receives the special matrix object created with 
+# makeCacheMatrix function as input and computes and caches its inverse. If 
+# the inverse has already been calculated, then the cacheSolve function 
+# retrieves the inverse from the cache. If the matrix has changed, cacheSolve 
+# calculates and caches the inverse again (when using the set function from 
+# makeCacheMatrix to change the input matrix the inverse is set to NULL).
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
         i <- x$getinv()
         if(!is.null(i)) {
                 message("getting cached data")
